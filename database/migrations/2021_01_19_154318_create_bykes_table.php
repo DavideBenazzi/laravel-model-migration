7659bc56476f5f704d17b59386f25cc67aeed3ce
Davide Benazzi <benazzi.davide@gmail.com>
2021-01-19 17:01:38 +0100

    Created bykes table

---
+++ database/migrations/2021_01_19_154318_create_bykes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBykesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bykes', function (Blueprint $table) {
+            $table->id();
+            $table->string('modello' , 50);
+            $table->string('marca' , 30);
+            $table->string('targa' , 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bykes');
+    }
+}
